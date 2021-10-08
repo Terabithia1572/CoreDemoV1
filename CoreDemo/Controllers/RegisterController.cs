9518cc3cdf72b385f760a76dbfb0a1f2c07e7b8d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2021-10-08 16:52:19 +0300

    CityList/Added

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,33 +15,59 @@ namespace CoreDemo.Controllers
     public class RegisterController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
+        WriterValidator wv = new WriterValidator();
 
         [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.getCity = GetCityList();
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(Writer p)
+        public IActionResult Index(Writer p, string city, string againPassword)
         {
-            WriterValidator wv = new WriterValidator();
+            //WriterValidator wv = new WriterValidator();
             ValidationResult results = wv.Validate(p);
-            if(results.IsValid)
+            if(results.IsValid && p.WriterPassword == againPassword)
             {
                 p.WriterStatus = true;
                 p.WriterAbout = "Deneme Test";
                 wm.WriterAdd(p);
                 return RedirectToAction("Index", "Blog");
             }
-          else
+            else if (!results.IsValid)
             {
-                foreach (var item in results.Errors)
+                foreach (var rule in results.Errors)
                 {
-                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                    ModelState.AddModelError(rule.PropertyName, rule.ErrorMessage);
                 }
+
+
+            }
+            else
+            {
+                ModelState.AddModelError("WriterPassword", "Hatalı giriş. Girilen şifreler eşleşmedi tekrar deneyiniz.");
             }
             return View();
+
+
         }
-    }
+        public List<string> GetCity()
+        {
+            String[] CityList = new String[] { "Adana", "Adıyaman", "Afyon", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Bartın", "Batman", "Balıkesir", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır", "Isparta", "İçel", "İstanbul", "İzmir", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Şırnak", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+            return new List<string>(CityList);
+        }
+
+        public List<SelectListItem> GetCityList()
+        {
+            List<SelectListItem> getCity = (from n in GetCity()
+                                            select new SelectListItem
+                                            {
+                                                Text = n,
+                                                Value = n
+                                            }).ToList();
+            return getCity;
+        }
+        }
 }
---
+++ CoreDemo/Models/WriterAndCityVM.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Models
+{
+    public class WriterAndCityVM
+    {
+
+        public List<CitiesVM> CitiesVMs { get; set; }
+
+        public List<Writer> Writers { get; set; }
+    }
+}
