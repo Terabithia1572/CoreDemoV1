b1519c36e5dbd1c1c7f8f3b4012cae5fc27cb9a2
Yunus İNAN <legendys65@gmail.com>
2021-10-24 16:32:35 +0300

    NewBlog/Added

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -1,7 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +15,16 @@ namespace CoreDemo.Controllers
     [AllowAnonymous]
     public class BlogController : Controller
     {
-        
+
         BlogManager bm = new BlogManager(new EfBlogRepository());
-        
+
         public IActionResult Index()
         {
 
             var values = bm.GetBlogListWithCategory();
             return View(values);
         }
-        
+
         public IActionResult BlogReadAll(int id)
         {
             ViewBag.i = id;
@@ -31,9 +34,42 @@ namespace CoreDemo.Controllers
 
         public IActionResult BlogListByWriter()
         {
-            var values=bm.GetBlogListByWriter(1);
+            var values = bm.GetBlogListByWriter(1);
             return View(values);
         }
-        
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p);
+            //WriterValidator wv = new WriterValidator();
+
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterID = 1;
+                bm.TAdd(p);
+
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else{ 
+                
+                foreach (var rule in results.Errors)
+                {
+                    ModelState.AddModelError(rule.PropertyName, rule.ErrorMessage);
+                }           
+            } 
+            return View();
+        }
+
     }
 }
