f1370adaa43658c9f74a77bfe9f4b4748c7a2468
Yunus İNAN <legendys65@gmail.com>
2021-11-09 17:03:17 +0300

    NotificationList/Added

--- DataAccessLayer/Migrations/20211109140015__mig_Noti_Color_add.Designer.cs
+++ DataAccessLayer/Migrations/20211109140015__mig_Noti_Color_add.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20211109140015__mig_Noti_Color_add")]
+    partial class _mig_Noti_Color_add
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
