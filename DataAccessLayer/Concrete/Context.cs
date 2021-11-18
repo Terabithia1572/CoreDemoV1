f2905dccd980b350a05ae38faace4e558af05593
Yunus İNAN <legendys65@gmail.com>
2021-11-18 16:23:11 +0300

    Team+Match/Message2+Writer/TablesAdded

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -8,13 +8,51 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-  public class Context:DbContext
+    public class Context : DbContext
     {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-5QBR47D;database=CoreBlogDB;integrated security=true;");
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Match>()
+                .HasOne(x => x.HomeTeam)
+                .WithMany(y => y.HomeMatches)
+                .HasForeignKey(z => z.HomeTeamID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            //IsRequired: ilgili alanın zorunluluğunu ifade eder. Bununla beraber IsOptional  opsiyonel olduğunu da belirtebiliriz.
+            // OnDelete yöntemi içinde 3 paremetre; (Silinme Durumunda)
+            //Cascade: Bağımlı olanlar siliniyor
+            //Restrict: Bağımlı olanlar etkilenmiyor
+            //SetNull: Değer NULL olarak değiştiriliyor.
+            modelBuilder.Entity<Match>()
+                .HasOne(x => x.GuestTeam)
+                .WithMany(y => y.AwayMatches)
+                .HasForeignKey(z => z.GuestTeamID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            modelBuilder.Entity<Message2>()
+                .HasOne(x => x.SenderUser)
+                .WithMany(y => y.WriterSender)
+                .HasForeignKey(z => z.SenderID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            modelBuilder.Entity<Message2>()
+                 .HasOne(x => x.ReceiverUser)
+                 .WithMany(y => y.WriterReceiver)
+                 .HasForeignKey(z => z.ReceiverID)
+                 .OnDelete(DeleteBehavior.ClientSetNull);
+
+            //HomeMatches-->WriterSender
+            //AwayMatches-->WriterReceiver
+
+            //HomeTeam-->SenderUser
+            //GuestTeam-->ReceiverUser
+        }
+
         public DbSet<About> Abouts { get; set; }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -25,6 +63,9 @@ namespace DataAccessLayer.Concrete
         public DbSet<BlogRayting> blogRaytings { get; set; }
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Message2> Message2s { get; set; }
 
     }
 }
