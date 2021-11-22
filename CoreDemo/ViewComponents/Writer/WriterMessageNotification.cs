ff21a21a39fe28c0aaed71734d12bdd3b1c03726
Yunus İNAN <legendys65@gmail.com>
2021-11-22 16:24:20 +0300

    Message+Include/Added

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -10,11 +10,11 @@ namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterMessageNotification:ViewComponent
     {
-        Message2Manager messageManager = new Message2Manager(new EfMessage2Repository());
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
         public IViewComponentResult Invoke()
         {
             int id = 2;
-            var values = messageManager.GetInboxListByWriter(id);
+            var values = mm.GetInboxListByWriter(id);
             return View(values);
         }
     }
--- DataAccessLayer/EntityFramework/EfMessage2Repository.cs
+++ DataAccessLayer/EntityFramework/EfMessage2Repository.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
 using System;
@@ -6,10 +7,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace DataAccessLayer.EntityFramework
 {
-  public  class EfMessage2Repository: GenericRepository<Message2>, IMessage2Dal
+  public class EfMessage2Repository: GenericRepository<Message2>, IMessage2Dal
     {
+        public List<Message2> GetListWithMessageByWriter(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.Message2s.Include(x => x.SenderUser).Where(x => x.ReceiverID == id).ToList();
+            }
+        }
     }
 }
