f9d8d67af4f77b1b27969d007b0d48645ddab4e8
Yunus İNAN <legendys65@gmail.com>
2022-01-15 22:26:18 +0300

    API/Entered

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,13 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "d8cbbf50300038d96d57b1e90deb2d71";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Van&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+
             return View();
         }
     }
